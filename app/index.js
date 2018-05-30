ee384fb04f14494231a118f9a592f4fc27297050
Sergio Rodriguez <oneandprime@gmail.com>
2018-05-29 22:42:36 -0400

    Initializing project

---
+++ app/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import Layout from './containers/Layout'
+import configureStore from './store'
+import startSagas from './sagas'
+import routes from './routes'
+
+export const store = configureStore()
+
+startSagas()
+
+const mapRouteToRouteComponent = r => <Route {...r} key={r.path} />
+
+ReactDOM.render(<div>HELLO WORLD!</div>, document.getElementById('app'))
+
+// ReactDOM.render(<div>
+//   <Provider store={store}>
+//     <HashRouter>
+//       <Layout>
+//         <Switch>
+//           {routes.map(mapRouteToRouteComponent)}
+//         </Switch>
+//       </Layout>
+//     </HashRouter>
+//   </Provider>
+// </div>, document.getElementById('app'))
